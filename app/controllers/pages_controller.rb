5f0b1a8dcfb28e83dd9cb9d19f04ff3b820ce303
Ghizlane Derdabi <derdabig@gmail.com>
2019-08-16 17:42:39 +0100

    new home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,8 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_catcher!, only: :home
 
   def home
+    @destinations = Destination.all
+    @catchers = Catcher.all
+    @stories = Story.all
   end
 end
