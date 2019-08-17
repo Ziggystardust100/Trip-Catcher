25737c10c848830f70087f5b05ce5a41937ebbd1
Andrea Rhiel <andrea.rhiel@yahoo.de>
2019-08-17 14:36:07 +0100

    added like Button to show destination.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   devise_for :catchers
   root to: 'pages#home'
-  resources :destinations, only: [:index,:show]
+  resources :destinations, only: [:index,:show]  do
+    resources :favourites, only: [:create]
+
+  end
   resources :trips, only: [:show, :new, :create, :destroy ]
   resources :catchers, only: [:edit, :index, :show, :update]
+  resources :favourites, only: [:destroy]
+
 end
