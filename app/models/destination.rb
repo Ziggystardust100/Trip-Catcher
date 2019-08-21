ccf6eab554833b3808ecbbb34eb3f775f667c1bc
Ghizlane Derdabi <derdabig@gmail.com>
2019-08-21 16:20:11 +0100

    navbar & destinations & seed & loginsignin

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -2,7 +2,7 @@ class Destination < ApplicationRecord
   has_many :trips
   has_many :favourites, dependent: :destroy
   has_many :invitations, through: :trips
-  has_many :stories
+  has_many :stories, dependent: :destroy
   has_many :catchers, through: :favourites
   has_many :pictures, dependent: :destroy
   validates :location, presence: true
