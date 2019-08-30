70f4c6631b2b1ecc8d11dc7b664852e501d594f2
Majid Jaidi <majid.jaidi@gmail.com>
2019-08-30 12:24:59 +0100

    messages websocket only delete on current_catcher

--- app/javascript/cable/messages.js
+++ app/javascript/cable/messages.js
@@ -1,27 +1,37 @@
+import createChannel from "./setup";
 
-import createChannel from './setup'
-
-const messages = document.getElementById('messages');
-const input = document.getElementById('message_content');
-
+const messages = document.getElementById("messages");
+const input = document.getElementById("message_content");
 
 if (messages) {
-  createChannel({channel: 'MessagesChannel', id: messages.dataset.id},{
-    received({message, catcher_username, message_time}) {
-      messages.insertAdjacentHTML('beforeend', htmlToInsert(message, catcher_username, message_time))
-      input.value = ''
-      messages.scrollTop = messages.scrollHeight
-
+  createChannel(
+    { channel: "MessagesChannel", id: messages.dataset.id },
+    {
+      received({ message, catcher_username, message_time }) {
+        messages.insertAdjacentHTML(
+          "beforeend",
+          htmlToInsert(message, catcher_username, message_time)
+        );
+        if (messages.dataset.catcherId === JSON.parse(message).catcher_id) {
+          input.value = "";
+        }
+        messages.scrollTop = messages.scrollHeight;
+      }
     }
-  });
+  );
 }
 
-
 function htmlToInsert(message, catcherUsername, messageTime) {
-  const m = JSON.parse(message)
-  const messageClass = parseInt(m.catcher_id) === parseInt(messages.dataset.catcherId) ? 'message-orange' : 'message-blue'
-  const timestampClass =  parseInt(m.catcher_id) === parseInt(messages.dataset.catcherId) ? 'message-timestamp-right' : 'message-timestamp-left'
-  return (`
+  const m = JSON.parse(message);
+  const messageClass =
+    parseInt(m.catcher_id) === parseInt(messages.dataset.catcherId)
+      ? "message-orange"
+      : "message-blue";
+  const timestampClass =
+    parseInt(m.catcher_id) === parseInt(messages.dataset.catcherId)
+      ? "message-timestamp-right"
+      : "message-timestamp-left";
+  return `
        <div class="${messageClass}">
           <p class='message-content'>
             <span>${m.content}</span>
@@ -32,5 +42,5 @@ function htmlToInsert(message, catcherUsername, messageTime) {
           </div>
         </div>
 
-  `)
+  `;
 }
--- app/javascript/cable/tripchats.js
+++ app/javascript/cable/tripchats.js
@@ -1,22 +1,32 @@
-import createChannel from './setup'
-const msg = document.getElementById('trip-msg');
-const chat = document.getElementById('tripchat_content');
-
+import createChannel from "./setup";
+const msg = document.getElementById("trip-msg");
+const chat = document.getElementById("tripchat_content");
 
 if (msg) {
-  createChannel({channel: 'TripchannelChannel', trip_id: msg.dataset.tripId},{
-    received({content, catcher_username}) {
-      msg.insertAdjacentHTML('beforeend', htmlChat(content, catcher_username))
-      chat.value = ''
-      msg.scrollTop = msg.scrollHeight
+  createChannel(
+    { channel: "TripchannelChannel", trip_id: msg.dataset.tripId },
+    {
+      received({ content, catcher_username }) {
+        msg.insertAdjacentHTML(
+          "beforeend",
+          htmlChat(content, catcher_username)
+        );
+        if (msg.dataset.catcherId === JSON.parse(content).catcher_id) {
+          chat.value = "";
+        }
+        msg.scrollTop = msg.scrollHeight;
+      }
     }
-  });
+  );
 }
 
 function htmlChat(content, catcherUsername) {
-  const c = JSON.parse(content)
-  const messageClass = parseInt(c.catcher_id) === parseInt(msg.dataset.catcherId) ? 'message-orange' : 'message-blue'
-  return (`
+  const c = JSON.parse(content);
+  const messageClass =
+    parseInt(c.catcher_id) === parseInt(msg.dataset.catcherId)
+      ? "message-orange"
+      : "message-blue";
+  return `
        <div class="${messageClass}">
           <div class='message-content'>
 
@@ -24,5 +34,5 @@ function htmlChat(content, catcherUsername) {
           </div>
         </div>
 
-  `)
+  `;
 }
